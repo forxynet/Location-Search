0e22a6417a847adf6315420f847dc35b37ab07e0
hakan.basturknewinn.com <forxynet@gmail.com>
2024-08-20 00:41:46 +0300

    updated locationsearch with button

--- src/components/LocationSearch.tsx
+++ src/components/LocationSearch.tsx
@@ -6,15 +6,15 @@ interface LocationSearchProps {
   onPlaceClick: (place: Place) => void;
 }
 
-export default function LocationSearch({onPlaceClick }: LocationSearchProps) {
+export default function LocationSearch({ onPlaceClick }: LocationSearchProps) {
   const [term, setTerm] = useState('');
   const [places, setPlaces] = useState<Place[]>([]);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
-   const results = await search(term);
-   setPlaces(results);
+    const results = await search(term);
+    setPlaces(results);
   }
 
   return <div>
@@ -22,12 +22,21 @@ export default function LocationSearch({onPlaceClick }: LocationSearchProps) {
       <label className='fond-bold' htmlFor='term'>
         Search
       </label>
-      <input 
-        className='border boder-gray-300 rounded-md shadow-sm focus:border-indigo-500 px-4 py-2 w-full'
-        id='term'
-        value={term}
-        onChange={e => setTerm(e.target.value)}
-      />           
+      <div className="grid grid-cols-4 gap-4">
+      <div className='col-span-3'>
+        <input
+          className='border boder-gray-300 rounded-md shadow-sm px-4 py-2 w-full'
+          id='term'
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+        />
+      </div>
+      <div className='border boder-gray-300 rounded-md shadow-sm py-2 text-center '>
+        <button className=''>
+          Go
+        </button>
+      </div>
+      </div>
     </form>
 
     <h1 className='font-bold mt-6'>Found Locations</h1>
@@ -37,7 +46,7 @@ export default function LocationSearch({onPlaceClick }: LocationSearchProps) {
           return <Fragment key={place.id}>
             <p className='text-sm'>{place.name}</p>
             <button className='bg-blue-500 text-xs text-white font-bold py-1 px-1 rounded'
-            onClick={() => onPlaceClick(place)}>
+              onClick={() => onPlaceClick(place)}>
               Go
             </button>
             <div className='border-b w-full col-span-2' />
